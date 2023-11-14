d4ecffc5880676a0bfa86ca864473b8e20d708bf
Jacob Beyer <jrbeyer@princeton.edu>
2023-11-14 16:31:21 -0500

    First try of pure pursuit, needs testing

--- single_agent_path_following.py
+++ single_agent_path_following.py
@@ -11,16 +11,19 @@ np.set_printoptions(precision=4,suppress=True)
 # constants
 # Environment refresh rate (from Holoocean, s)
 env_refresh_rate = 1/30
+
+control_ticks_per_update = 5
 # Control rate (s)
-control_rate = 1/30
+control_rate = env_refresh_rate * control_ticks_per_update
+
 
 pitch_kp = 1.1
-pitch_ki = 0.001
+pitch_ki = 0.1
 pitch_kd = 3
 pitch_integrator = 0
-pitch_i_saturation = 1
+pitch_i_saturation = 3
 pitch_prior_error = 0
-pitch_command_clip = 2
+pitch_command_clip = 6
 
 yaw_kp = 1.1
 # yaw_ki = 0.0025
@@ -115,7 +118,7 @@ def pitch_loop(pitch_current, pitch_desired) -> Tuple[np.ndarray, float, float, 
     nominal_command = np.ndarray(8)
     nominal_command.fill(0)
 
-    unit_rotation_command = np.array([-1,-1,1,1,0,0,0,0])
+    unit_rotation_command = np.array([1,1,-1,-1,0,0,0,0])
 
     # constrain pitch_error to -180 to 180
     pitch_error = pitch_desired - pitch_current
@@ -136,59 +139,99 @@ def pitch_loop(pitch_current, pitch_desired) -> Tuple[np.ndarray, float, float, 
 
     return (pitch_pid_out * unit_rotation_command, pitch_error, pitch_pid_out, pitch_proportional, pitch_integrator, pitch_derivative)
 
+# pure pursuit law
+# adapted from Pelizer (2017)
+# W_i: initial position of line to track
+# W_i1: final position of line to track
+# position: current position of AUV
+# output: (yaw_desired, pitch_desired)
+def pure_pursuit(W_i, W_i1, position) -> Tuple[float, float]:
+    alpha = np.arctan2(W_i1[1]-W_i[1], W_i1[0]-W_i[0])
+    yaw_desired = np.arctan2(W_i1[2]-position[2], W_i1[1]-position[1])*180/np.pi
+    
 
+    Rz = np.array([[np.cos(alpha),  np.sin(alpha),  0],
+                   [-np.sin(alpha), np.cos(alpha),  0],
+                   [0,              0,              1]])
+    
+    W_i1_r = Rz @ W_i1
+    position_r = Rz @ position
+    # print(f'Position: {position}')
+    # print(f'Rz: \n{Rz}')
+    # print(f'Position_r: {position_r}')
+
+    pitch_desired = np.arctan2(position_r[2]-W_i1_r[2], W_i1_r[0]-position_r[0])
+
+    # W_i_r = Rz*W_i
+    # beta = np.arctan2(W_i_r[2]-W_i1_r[2], 
+    #                   np.sqrt((W_i_r[0]-W_i1_r[0])**2 + (W_i_r[1]-W_i1_r[1])**2))
+
+    # Ry = np.array([[np.cos(beta),   0,              -np.sin(beta)],
+    #                [0,              1,              0],
+    #                [np.sin(beta),   0,              np.cos(beta)]])
+
+    # e = Ry*Rz*(position - W_i)
+    
+    return (yaw_desired[0], pitch_desired[0])
+
+
+VERBOSE = True
 
 if __name__ == "__main__":
     with holoocean.make("PierHarbor-Hovering") as env:
         env.set_render_quality(1)
-        nominal_command = np.ndarray(8)
-        nominal_command.fill(0)
+        # nominal_command = np.ndarray(8)
+        # nominal_command.fill(0)
+        nominal_command = np.array([0,0,0,0,6,6,6,6])
         command = nominal_command
-        yaw_desired = -90
-        pitch_desired = -4
+        yaw_desired = 0
+        pitch_desired = 0
+
+        state = env.step(command)
+        W_i = state["PoseSensor"][0:3,3]
+        W_i1 = W_i + np.array([100,30,-16])
+
+
         for i in range(100000):
             if 'q' in pressed_keys:
                 break
             state = env.step(command)
             # time.sleep(0.1)
 
-            orientation = state["PoseSensor"][0:3,0:3]
-            fx = orientation[0,0]
-            fy = orientation[0,1]
-            uz = orientation[2,2]
-            yaw = np.arctan2(fx, fy)*180/np.pi
-            pitch = np.arccos(-uz)*180/np.pi
-            yaw_command, yaw_error, yaw_pid_out, yaw_proportional, yaw_integrator, yaw_derivative = yaw_loop(yaw, yaw_desired)
-            pitch_command, pitch_error, pitch_pid_out, pitch_proportional, pitch_integrator, pitch_derivative = pitch_loop(pitch, pitch_desired)
+            if i % control_ticks_per_update == 0:
+                orientation = state["PoseSensor"][0:3,0:3]
+                position = state["PoseSensor"][0:3,3].reshape(-1,1)
+                fx = orientation[0,0]
+                fy = orientation[0,1]
+                fz = orientation[0,2]
+                yaw = np.arctan2(fx, fy)*180/np.pi
+                pitch = np.arcsin(-fz)*180/np.pi
+                yaw_desired, pitch_desired = pure_pursuit(W_i, W_i1, position)
+                yaw_command, yaw_error, yaw_pid_out, yaw_proportional, yaw_integrator, yaw_derivative = yaw_loop(yaw, yaw_desired)
+                pitch_command, pitch_error, pitch_pid_out, pitch_proportional, pitch_integrator, pitch_derivative = pitch_loop(pitch, pitch_desired)
+                command = nominal_command + pitch_command + yaw_command
 
-            command = nominal_command + yaw_command + pitch_command
-
-            if i % 10 == 0:
-                print(f'Yaw desired: {yaw_desired}')
-                print(f'Yaw: {yaw:.6}')
-                print(f'Yaw error: {yaw_error:.6}')
-                print(f'Yaw proportional: {yaw_proportional:.6}')
-                print(f'Yaw integrator: {yaw_integrator:.6}')
-                print(f'Yaw derivative: {yaw_derivative:.6}')
-                print(f'Yaw PID out: {yaw_pid_out:.6}')
-                print(f'Pitch desired: {pitch_desired}')
-                print(f'Pitch: {pitch:.6}')
-                print(f'Pitch error: {pitch_error:.6}')
-                print(f'Pitch proportional: {pitch_proportional:.6}')
-                print(f'Pitch integrator: {pitch_integrator:.6}')
-                print(f'Pitch derivative: {pitch_derivative:.6}')
-                print(f'Pitch PID out: {pitch_pid_out:.6}')
-                print(f'Yaw command: {yaw_command}')
-                print(f'Pitch command: {pitch_command}')
+            if i % (5*control_ticks_per_update) == 0:
+                print(f'Iteration: {i}')
                 print(f'Command: {command}\n')
-
-
-            # if i % control_rate == 0:
-            #     print(f'State: {state}')
-            #     print(f'Pose sensor: \n{state["PoseSensor"]}')
-            #     # PoseSensor is a 4x4 array, with the first 3x3 being the rotation matrix, next 3x1 being x,y,z, and bottom row being 0,0,0,1
-            #     print(f'Orientation matrix: \n{state["PoseSensor"][0:3,0:3]}')
-            #     print(f'Position: \n{state["PoseSensor"][0:3,3]}')
-            #     command = control_law()
+                if VERBOSE:
+                    # print(f'Orientation matrix: \n{state["PoseSensor"][0:3,0:3]}')
+                    print(f'Position: {position}')
+                    print(f'Yaw desired: {yaw_desired}')
+                    print(f'Yaw: {yaw:.6}')
+                    print(f'Yaw error: {yaw_error:.6}')
+                    print(f'Yaw proportional: {yaw_proportional:.6}')
+                    print(f'Yaw integrator: {yaw_integrator:.6}')
+                    print(f'Yaw derivative: {yaw_derivative:.6}')
+                    print(f'Yaw PID out: {yaw_pid_out:.6}')
+                    print(f'Pitch desired: {pitch_desired}')
+                    print(f'Pitch: {pitch:.6}')
+                    print(f'Pitch error: {pitch_error:.6}')
+                    print(f'Pitch proportional: {pitch_proportional:.6}')
+                    print(f'Pitch integrator: {pitch_integrator:.6}')
+                    print(f'Pitch derivative: {pitch_derivative:.6}')
+                    print(f'Pitch PID out: {pitch_pid_out:.6}')
+                    print(f'Yaw command: {yaw_command}')
+                    print(f'Pitch command: {pitch_command}')
 
 
